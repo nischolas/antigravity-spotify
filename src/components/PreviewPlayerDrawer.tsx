40d29e5fd4b5fed9b3a1542f71e85e44f6912ede
Nicholas Mathieu <nic.schneider@ymail.com>
2026-02-21 23:47:09 +0100

    feat: Make close btn icon only

--- src/components/PreviewPlayerDrawer.tsx
+++ src/components/PreviewPlayerDrawer.tsx
@@ -57,8 +57,7 @@ export const PreviewPlayerDrawer: React.FC<PreviewPlayerDrawerProps> = ({ trackU
           <button className="preview-drawer-link" onClick={onClose} aria-label="Close player">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>{" "}
-            {t("common.close")}
+            </svg>
           </button>
         </div>
       </div>
